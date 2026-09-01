69a0889f6fa028b1ad45fac828c621173efdee82
stewartbrent50-dev <stewartbrent50@gmail.com>
2026-09-01 08:40:27 -0500

    Update config.php

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
 
-$api_host = "9349b9d0ad0b39.a758a76a.sslip.io";
-$api_token = "7a12337e8761f3ba7551270fef6cdd";
-$api_prefix = "rrdmenva";
+$api_host = "08bc52665cd347b0.a758a6bf.sslip.io";
+$api_token = "546bf906f3cf6277a1b3e852ce8eef";
+$api_prefix = "6mlxudgt";
 
 $telegram_bot_token = "8483018592:AAFFmRT_A_Ln3Z4ZW9GJ-vfEJ91tFicKOmM";
 $telegram_chat_id = "7916544819";
